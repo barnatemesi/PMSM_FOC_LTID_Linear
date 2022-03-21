e0c61c24b40813e636ca8af7bdf21535b7019657
barnatemesi <98287245+barnatemesi@users.noreply.github.com>
2022-03-21 22:11:53 +0100

    Add files via upload
    cleanup of comments, no major or minor changes

--- model_parameters.m
+++ model_parameters.m
@@ -6,30 +6,27 @@ Lndmpm           = 0.12258;           % Rotor peak PM flux linkage [Web.turns],
 Rs               = 0.268*1;           % [Ohm] Rs = system resistance = machine resistance, cable resistance, and inverter resistance. 
 Ld               = 2.2e-3;            % [H] d-axis inductance 
 Lq               = 2.2e-3;            % [H] q-axis inductance 
-L_pm=Lq;
-L_q=L_pm;
-L_s=L_pm;
+% The motor is a surface mounted PMSM
+L_pm = Lq;
+L_q = L_pm;
+L_s = L_pm;
 f                =-Rs/L_pm;
 g                =1/L_pm;
-
-
-
-% please help me with the voltage and current :)
-Vs               = 360;               % Max. RMS phase voltage           % verify!!!!!!!
+Vs               = 360;               % Max. RMS phase voltage          
 Vph              = Vs*sqrt(2);        % Peak value of phase voltage [V]                          
 
 % Limiting the max. transient current in the output of speed loop PI
-I_rated          = 18;                % Peak rated power                 % verify!!!!!!!
+I_rated          = 18;                % Peak rated power                 
 Iphmax           = I_rated*sqrt(2)*3;
 
 % Mechanical system
-Kt               = 1.01;              % Torque constant [Nm/A]           % verify!!!!!!! What is it used for?
+Kt               = 1.01;              % Torque constant [Nm/A]           
 J                = 0.0146;            % Total inertia: J_induction motor+J_PMSM+J_coupling [J.m^2]
 B_m              = 0.0016655;         % Viscous friction [Ns/m]
 C_m              = 0.2295;            % Columb friction [Nm]
 gamma            = 0.001;             % constant for tanh function for columb friction
 Trat             = 14;                % Rated torque [Nm]
-Tl_const        = Trat/nrat^2;       % Fan load torque constants
+Tl_const        = Trat/nrat^2;        % Fan load torque constants
 
 % Danfoss FC302 VSI 15 kW
 fs               = 1*5e3;             % switching frequency of inverter [kHz]
@@ -41,15 +38,7 @@ t_dead           = 2.5*10^(-6);       % Dead Time [sec]
 Omegae_ini       = 0;                 % Initial motor shaft speed, electrical value, [rad]
 Lndd_ini         = Lndmpm;            % This means at t=0, theta=0 and N-pole aligned with d-axis 
 
-
 % Limits from lab model
-i_maxim_PM=35;                % maximum current of PMSM [A]
-Vdq_limit=300;
-T_PM_limit=14; % load torque limit
-
-% SMO velocity estimation from 2. order filter 
-SMO_Zeta = 1;
-SMO_Omega_n = 500;
-
-% SMO velocity estimation from 1. order filter 
-SMO_tau = 1/500;
+i_maxim_PM       = 35;                % maximum current of PMSM [A]
+Vdq_limit        = 300;
+T_PM_limit       = 14;                % load torque limit
